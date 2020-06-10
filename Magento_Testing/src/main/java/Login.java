4ca8d4ba2d8d50d8a85f393390bae15c74e81e44
SUDHIR <SUDHIR@192.168.2.10>
2020-06-10 20:39:21 +0530

    first commit

---
+++ Magento_Testing/src/main/java/Login.java
@@ -0,0 +1,28 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login
+{
+  WebDriver driver;
+  public Login(WebDriver driver)
+  {
+	  this.driver=driver;
+  }
+  By email=By.id("email");
+  By pwd=By.id("pass");
+  By login=By.id("send2");
+  public void typeEmail(String un)
+  {
+	  driver.findElement(email).sendKeys(un);
+  }
+  public void typePassword(String pw)
+  {
+	  driver.findElement(pwd).sendKeys(pw);
+  }
+  public void clickOnLogin()
+  {
+	  driver.findElement(login).click();
+  }
+}
---
+++ Magento_Testing/src/test/java/MagentoTest.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest
+{
+	@Test
+    public void positivecredentials()
+	{
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\SUDHIR\\Desktop\\SeleniumSoftwares\\chromedriver83.exe");
+    	WebDriver driver=new ChromeDriver();
+    	driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+    	driver.get("https://www.magento.com");
+    	
+    	Welcome w=new Welcome(driver);
+    	w.clickonMyAccount();
+    	
+    	Login l=new Login(driver);
+    	l.typeEmail("ihassan395@gmail.com");
+    	l.typePassword("Welcome@123");
+    	l.clickOnLogin();
+		
+    	Home h=new Home(driver);
+        h.clickOnLogout();
+	}
+
+}
